84cf9ad6af4941e3466114c275f0a5496ca1193f
Panachai <panachai.ny@gmail.com>
2017-05-22 21:14:39 +0700

    origin

---
+++ CustomView3_Venio/Model/CustomerModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CustomView3_Venio {
+	public class CustomerModel {
+
+		public string CustomerName { get; set; }
+		public string CustomerContact { get; set; }
+		public string CustomerAddress { get; set; }
+
+	}
+}
---
+++ CustomView3_Venio/Model/DemoActivityModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CustomView3_Venio {
+	public class DemoActivityModel {
+
+		public string ImvDisplay { get; set; }
+		public string TxtTime { get; set; }
+
+		public string TxtCustomerN { get; set; }
+		public string TxtCustomerStatement { get; set; }
+		public string TxtCustomerDescription { get; set; }
+	}
+}
